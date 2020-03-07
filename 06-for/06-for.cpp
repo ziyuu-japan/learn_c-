e7fc6fe72d6d47b779342fec29c299f15a7bfdfd
ziyuu-japan <furutain@yahoo.co.jp>
2020-03-07 14:41:44 +0900

    c++11でコンパイルする方法をメモ

---
+++ 06-for/06-for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+  vector<int> v{2,3,5,7}; ;;;;
+
+  //size_t s = v.size();
+  //for(size_t i = 0; i < s; i++){
+  //  cout << v[i] << ",";
+  //}
+  cout << endl;
+}
