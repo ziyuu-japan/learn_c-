5d6b299a2f83f19f3a019f2b845d7458025dcda2
ziyuu-japan <furutain@yahoo.co.jp>
2020-03-07 13:19:37 +0900

    インクルードガードなど

---
+++ 05-number/05-number.cpp
@@ -0,0 +1,9 @@
+#include "number.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << numOfDivisors(10) << endl;
+  cout << fibonacci(10) << endl;
+  cout << factorial(10) << endl;
+}
---
+++ 05-number/number.h
@@ -0,0 +1,8 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+int numOfDivisors(int n);
+int fibonacci(int n);
+int factorial(int n);
+
+#endif
